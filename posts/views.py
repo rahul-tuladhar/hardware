2e9e47c73c5eede4027ef58639f162cb1d60b553
Rahul Tuladhar <rt4hc@virginia.edu>
2018-02-19 18:48:52 -0500

    Jsonresponse not done yet

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Post
 from .forms import PostEditForm
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 def index(request):
+    # response = JsonResponse(some_dictionary)
     context = {}
     all_posts = Post.objects.all()
+    
     context['posts'] = all_posts
-    return render(request, 'index.html', context)
+    return JsonResponse(context)
 
 def post_detail(request, post_title=None):
     context = {}
@@ -16,28 +20,21 @@ def post_detail(request, post_title=None):
             post = Post.objects.get(title=post_title)
             context['post'] = post
         except ObjectDoesNotExist:
-            return render(request, 'error.html')
-    return render(request, 'post_detail.html', context)
+            context = {'error': 'object does not exist'}
+    return JsonResponse(context)
 
 def edit_post(request, post_title=None):
-    context = {}
+    # context = {}
+    if request.method == 'GET':
+        return JsonResponse({'form': 'form is blank'})
     if request.method == 'POST':
-        try:
-            form = PostEditForm(request.POST)
-            if form.is_valid():
-                context['form'] = form
-                form.save()
-                return render(request, 'index.html', context)
-            else:
-                return render(request, 'index.html', context)
-        except ObjectDoesNotExist:
-            return render(request, 'error.html')
-    else:
-        try:
-            post = Post.objects.get(title=post_title)
-            context['post'] = post
-            form = PostEditForm(request.POST, instance=post)
-            context['form'] = form
-        except ObjectDoesNotExist:
-            return render(request, 'error.html')
-        return render(request, 'edit_post.html', context)
+        return JsonResponse({'form': 'form submitted successfully'})
+    # else:
+    #     try:
+    #         post = Post.objects.get(title=post_title)
+    #         context['post'] = post
+    #         form = PostEditForm(request.POST, instance=post)
+    #         context['form'] = form
+    #     except ObjectDoesNotExist:
+    #         return render(request, 'error.html')
+    #     return render(request, 'edit_post.html', context)
