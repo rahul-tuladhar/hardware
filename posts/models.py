46ba6fa4c9da96e762637ff0959acd1416e4763b
Rahul Tuladhar <rt4hc@virginia.edu>
2018-03-03 22:37:30 -0500

    Added a few attributes to the models

--- posts/models.py
+++ posts/models.py
@@ -4,13 +4,18 @@ from login.models import Profile, Group
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+	# 
+	author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     date = models.DateTimeField(default=timezone.now)
-    description = models.TextField(max_length=2048)
+	description = models.TextField(max_length=2048)
+    title = models.CharField(max_length=50)
     image = models.ImageField(blank=True, null=True)
-    price = models.FloatField(default=1.0)
     location = models.CharField(default='JPA', max_length=24)
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    part = models.CharField(max_length=50)1
+    payment_method = models.CharField(max_length)
+    price = models.FloatField(default=1.0)
+    transaction_type = models.CharField(max_length=50)
+    
 
     def __str__(self):
         return self.title
--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from djanog.core.urlresolvers import reverse
+from petsitting.models import Posts, Reviews
 # Create your tests here.
+
+# class GetReviewPageTestCase(TestCase):
+# 	# setUp() is called before each test in this class
+# 	def setUp(self):
+# 		pass
+
+# 	def success_response(self):
+# 		# .client initiates a dummy browser
+# 		response = self.client.get()
+
+# 		#Checks that the HTTP response code is 200
+# 		self.assertContains(response, '')
+
+# 	def fails_invalid(self):
+# 		response = self.client.get()
+# 		self.assertEquals(response.status_code, 404)
+
+# 	# tearDown() is called at the end of each test
+# 	def tearDown(self):
+# 		pass
