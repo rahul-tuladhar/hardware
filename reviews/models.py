7ecdce7a0ca4e52dc68560ced95530e216a78714
Rahul Tuladhar <rt4hc@virginia.edu>
2018-03-03 22:39:41 -0500

    Model changes

--- reviews/models.py
+++ reviews/models.py
@@ -5,13 +5,14 @@ from django.utils import timezone
 
 # Create your models here.
 class Review(models.Model):
-    title = models.CharField(max_length=50)
-    rating = models.FloatField(default=5.0)
-    upvotes = models.IntegerField(default=0)
-    date = models.DateTimeField(default=timezone.now)
+	date = models.DateTimeField(default=timezone.now)
     description = models.TextField(max_length=2048)
     post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    rating = models.FloatField(default=5.0)
     reviewer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    upvotes = models.IntegerField(default=0)
+    
 
     def __str__(self):
         return self.title
