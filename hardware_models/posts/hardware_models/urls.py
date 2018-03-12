c0e40494a20e55a20089f07957259058da9faf0b
jason <not.j.deng@gmail.com>
2018-03-11 22:08:30 -0400

    refactor

--- hardware_models/posts/hardware_models/urls.py
+++ hardware_models/posts/hardware_models/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import url, include
 from django.contrib import admin
-# from .views import
+from django.conf.urls import url, include
+from posts.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^user/', include('login.urls')),
     url(r'^posts/', include('posts.urls')),
-    url(r'^reviews/', include('reviews.urls')),
 ]
