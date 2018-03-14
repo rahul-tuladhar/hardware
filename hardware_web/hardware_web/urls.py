55041e2e272572f976c4daf3fa9b774d2de881c2
jason <not.j.deng@gmail.com>
2018-03-14 19:12:48 -0400

    bootstrap + urls

--- hardware_web/hardware_web/urls.py
+++ hardware_web/hardware_web/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 # from .views import
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     # url(r'^v3/user/', include('login.urls')),
     url(r'^v3/posts', include('posts.urls')),
     # url(r'^v3/reviews/', include('reviews.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
