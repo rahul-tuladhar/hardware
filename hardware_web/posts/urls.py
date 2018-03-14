d0e92999b26b1dd7a6619f27f98750464eac6050
jason <not.j.deng@gmail.com>
2018-03-14 18:20:52 -0400

    fixed a homepage url

--- hardware_web/posts/urls.py
+++ hardware_web/posts/urls.py
@@ -3,6 +3,6 @@ from .views import homepage
 from .views import post_detail
 
 urlpatterns = [
-    url(r'^/home/', homepage, name='home'),
+    url(r'^/home/$', homepage, name='home'),
     url(r'^/(?P<id>\d+)/$', post_detail, name='post_detail')
 ]
