e88bf23fd0e122e614e0fde3558855c45273ec60
jason <not.j.deng@gmail.com>
2018-03-16 15:47:09 -0400

    removed reviews and login apps and all of their references

--- hardware_models/hardware_models/settings.py
+++ hardware_models/hardware_models/settings.py
@@ -31,9 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'login.apps.LoginConfig',
     'posts.apps.PostsConfig',
-    'reviews.apps.ReviewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
