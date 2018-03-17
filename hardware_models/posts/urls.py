0c9e63697bb8f09d593574f2ccf04d8168495b72
Katherine Yan <katherine.yan2015@gmail.com>
2018-03-17 17:50:50 -0400

    minor bug fixes

--- hardware_models/posts/urls.py
+++ hardware_models/posts/urls.py
@@ -4,5 +4,4 @@ from .views import *
 urlpatterns = [
     url(r'^home/$', home, name='home'),
     url(r'^post_detail/(?P<id>[0-9]+)$', post_detail, name='post_detail'),
-    url(r'^edit_post/(?P<id>[0-9]+)$', edit_post, name='edit_post'),
 ]
--- hardware_models/posts/views.py
+++ hardware_models/posts/views.py
@@ -60,36 +60,36 @@ def post_detail(request, id):
     return JsonResponse(post_dict, safe=False)
 
 
-def edit_post(request, id):
-    """
-    Returns a JsonResponse dictionary of a post during a POST Request.
-    :param request:
-    :param post_title:
-    :param post_author:
-    :param post_description:
-    :param post_price:
-    :return:
-    """
-    if request.method == 'GET':
-        post_dict = {'status': 'should not get request edit_post'}
-    # TODO: Fully implement the entity API layer of this method
-    if request.method == 'POST':
-        try:
-            post = Post.objects.get(id=id)
-            # if (request.POST is not None):
-            #     post.title = post_title
-            # if (post_description is not None):
-            #     post.description = post_description
-            # if (post_author is not None):
-            #     post.author = post_author
-            # if (post_price is not None):
-            #     post.price = float(post_price)
-            # post.save()
-            post_dict = model_to_dict(post)
-            del post_dict['image']
-        except ObjectDoesNotExist:
-            post_dict = {'status': 'ObjectDoesNotExist'}
-    return JsonResponse(post_dict, safe=False)
+# def edit_post(request, id):
+#     """
+#     Returns a JsonResponse dictionary of a post during a POST Request.
+#     :param request:
+#     :param post_title:
+#     :param post_author:
+#     :param post_description:
+#     :param post_price:
+#     :return:
+#     """
+#     if request.method == 'GET':
+#         post_dict = {'status': 'should not get request edit_post'}
+#     # TODO: Fully implement the entity API layer of this method
+#     if request.method == 'POST':
+#         try:
+#             post = Post.objects.get(id=id)
+#             # if (request.POST is not None):
+#             #     post.title = post_title
+#             # if (post_description is not None):
+#             #     post.description = post_description
+#             # if (post_author is not None):
+#             #     post.author = post_author
+#             # if (post_price is not None):
+#             #     post.price = float(post_price)
+#             # post.save()
+#             post_dict = model_to_dict(post)
+#             del post_dict['image']
+#         except ObjectDoesNotExist:
+#             post_dict = {'status': 'ObjectDoesNotExist'}
+#     return JsonResponse(post_dict, safe=False)
 
 
 
