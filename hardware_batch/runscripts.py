5f4c1241f1c59552bd788801011939b46199a480
Rahul Tuladhar <rt4hc@virginia.edu>
2018-04-09 18:48:32 -0400

    fixtures work again with search

--- hardware_batch/runscripts.py
+++ hardware_batch/runscripts.py
@@ -2,15 +2,23 @@ from kafka import KafkaConsumer
 from elasticsearch import Elasticsearch
 import time
 import json
-#
-time.sleep(30)
-#
-es = Elasticsearch(['es'])
-consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+
+
+
+# Tries to get consumer freeze process if not able
 while True:
+    try:
+        consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+        es = Elasticsearch(['es'])
+        break
+    except:
+        time.sleep(5)
+
+
+while True:
+    print('true')
     for message in consumer:
-        print(json.loads(message.value).decode('utf-8'))
-        new_consumer = json.loads(message.value).decode('utf-8')
-        es.index(index='listing_index', doc_type='listing', id=new_consumer['id'],
-             body=new_consumer)
-    es.indices.refresh(index="listing_index")
+        new_consumer = json.loads((message.value).decode('utf-8'))
+        print(json.loads((message.value).decode('utf-8')))
+        es.index(index='listing_index', doc_type='listing', id=new_consumer['result']['id'], body=new_consumer['result'])
+        es.indices.refresh(index="listing_index")
