03cce36ffd680c531d09caeb6810d9341e1b0fb5
Katherine Yan <katherine.yan2015@gmail.com>
2018-05-07 17:13:45 -0400

    finished recommendation system

--- data/spark.py
+++ data/spark.py
@@ -1,45 +1,62 @@
 from pyspark import SparkContext
-# import _mysql
-
-# db=_mysql.connect(host="db",user="www",
-#                   passwd="$3cureUS",db="cs4501") 
+import MySQLdb
 
 
-
-
+#------------------- SPARK RECOMMENDATION SECTION ----------------------#
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
+data = sc.textFile("/tmp/data/access.log", 2)  	# each worker loads a piece of the data file   			
 
-data = sc.textFile("/tmp/data/access.log", 2)     		# each worker loads a piece of the data file
+#------ Read data in as pairs of (user_id, item_id clicked on by the user) ----------#
+user_views = data.map(lambda line: line.split("\t"))	# tell each worker to split each line of it's partition					
+user_views = user_views.mapValues(lambda id: int(id))	# turn the values into ints	
 
-pairs = data.map(lambda line: line.split("\t"))   		# tell each worker to split each line of it's partition
-user_list = pairs.groupByKey();							# group data into (user_id, list of item ids they clicked on)
+#------ Group data into (user_id, list of item ids they clicked on) ----------#
+user_list = user_views.groupByKey()		
 
+#------ Transform into (user_id, (item1, item2) where item1 and item2 are pairs of items the user clicked on ----------#			
+pairs = user_list.mapValues(lambda items: [(x,y) for x in items for y in items if x != y])	# map values into pairs where every value is paired with another if they aren't equal
+pairs = pairs.mapValues(lambda items: [(x,y) for x,y in items if x >= y])					# order pairs so highest value comes first
+pairs = pairs.flatMap(lambda line: [ (line[0], line[1][i]) for i in range(len(line[1])) ] )	# split into multiple RDDs 										# switch user_id to be the value
+pairs = pairs.map(lambda line: (line[1], line[0]))											# switch user id to be value
 
+#------ Transform into ((item1, item2), list of user1, user2 etc) where users are all the ones who co-clicked (item1, item2) ----------#
+views = pairs.distinct().groupByKey()		# group by page pairs that are distinct
 
+#------ Transform into ((item1, item2), count of distinct users who co-clicked (item1, item2) ----------#
+count = views.map(lambda line: (line[0], len(line[1])))	
 
+#------ Filter out any results where less than 3 users co-clicked the same pair of items ----------#
+final = count.filter(lambda line: line[1] >= 3)
 
-output = user_list.collect() 				  		
+#------ Collect results from workers ----------#
+output = final.collect() 
 
-for page_id, count in output:
-	print("user_id %s items: " % page_id)
-	print(list(count))
-
-print ("Popular items done")
-
-# pages = pairs.map(lambda pair: (pair[1], 1))      		# re-layout the data to ignore the user id
-# count = pages.reduceByKey(lambda x,y: int(x)+int(y))    # shuffle the data so that each key is only on one worker
-#                                                   		# and then reduce all the values by adding them together
-# output = count.collect()                          		# bring the data back to the master node so we can print it out
 # for page_id, count in output:
-#     print ("page_id %s count %d" % (page_id, count))
+# 	print("pages: ")
+# 	print(list(page_id))
+# 	print(count)
 # print ("Popular items done")
 
 sc.stop()
 
 
-# Read data in as pairs of (user_id, item_id clicked on by the user)
-# 
-# Transform into (user_id, (item1, item2) where item1 and item2 are pairs of items the user clicked on
-# Transform into ((item1, item2), list of user1, user2 etc) where users are all the ones who co-clicked (item1, item2)
-# Transform into ((item1, item2), count of distinct users who co-clicked (item1, item2)
-# Filter out any results where less than 3 users co-clicked the same pair of items
+#------------------- SQL DATABASE SECTION ----------------------#
+db = MySQLdb.connect(host="db",user="www", passwd="$3cureUS",db="cs4501") 
+c = db.cursor()
+
+# use the correct table
+c.execute("use cs4501;")
+
+
+
+
+
+
+
+
+
+
+db.close()
+
+
+
