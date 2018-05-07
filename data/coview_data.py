b2e3fef5262883db1b62354316578cfea7ec9418
Rahul Tuladhar <rt4hc@virginia.edu>
2018-05-07 18:46:56 -0400

    some things dont work for the coview

---
+++ data/coview_data.py
@@ -0,0 +1,26 @@
+from kafka import KafkaConsumer
+import time
+import json
+
+while True:
+    try:
+        consumer = KafkaConsumer('co-view-topic', bootstrap_servers=['kafka:9092'])
+        print(consumer)
+        break
+    except:
+        time.sleep(5)
+print('true1')
+
+while True:
+    print('true2')
+    with open("data.txt", "a+") as data:
+        print('true3')
+        for message in consumer:
+            new_consumer = json.loads(message.value.decode('utf-8'))
+            print(new_consumer)
+            print(new_consumer['result']['id'])
+            print(new_consumer['user_id'])
+            item_id = new_consumer['result']['id']
+            user_id = new_consumer['user_id']
+            data.write("%s \t %s" % (user_id, item_id))
+    data.close()
