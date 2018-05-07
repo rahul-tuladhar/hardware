6a45cc2c486dac0abdf6396f07baacbdde6d16e6
Katherine Yan <katherine.yan2015@gmail.com>
2018-05-07 19:40:48 -0400

    script to automate spark

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker exec -it spark-worker bash -c "/data/pymysql.py"
-docker exec -it spark-master bash -c "/data/pymysql.py"
+docker exec -it spark-worker bash -c "/tmp/data/pymysql.py"
+docker exec -it spark-master bash -c "/tmp/data/pymysql.py"
 
-
-watch -n 120 docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/spark.py
+while [[true]]; do
+	docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/spark.py
+	sleep 120
+done
