8e9fd52e2fe7a3a55ff3693ce7e04f19ccb13afb
Katherine Yan <katherine.yan2015@gmail.com>
2018-05-07 20:04:22 -0400

    shell script working

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-docker exec -it spark-worker bash -c "/tmp/data/pymysql.py"
-docker exec -it spark-master bash -c "/tmp/data/pymysql.py"
+docker exec -it spark-worker bash -c "apt-get update &&
+apt-get install python3-dev libmysqlclient-dev -y &&
+apt-get install python-pip -y &&
+pip install mysqlclient &&
+apt-get install python-mysqldb"
+docker exec -it spark-master bash -c "apt-get update &&
+apt-get install python3-dev libmysqlclient-dev -y &&
+apt-get install python-pip -y &&
+pip install mysqlclient &&
+apt-get install python-mysqldb"
 
-while [[true]]; do
+while [[ true ]]; do
 	docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/spark.py
 	sleep 120
 done
